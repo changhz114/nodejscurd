41c8c74fe8a53cb49be9f4d21562baf6b0257ada
chz <changhengz@student.unimelb.edu.au>
2023-03-21 14:03:26 +1100

    fix errors

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,14 +1,11 @@
 const userResolvers = require('./userResolvers');
-const postResolvers = require('./postResolvers');
 
 const resolvers = {
   Query: {
     ...userResolvers.Query,
-    ...postResolvers.Query,
   },
   Mutation: {
     ...userResolvers.Mutation,
-    ...postResolvers.Mutation,
   },
 };
 
--- src/schema/typeDefs.js
+++ src/schema/typeDefs.js
@@ -8,27 +8,15 @@ const typeDefs = gql`
     age: Int
   }
 
-  type Post {
-    id: ID!
-    title: String!
-    content: String!
-    author: String
-  }
-
   type Query {
     users: [User]
     user(id: ID!): User
-    posts: [Post]
-    post(id: ID!): Post
   }
 
   type Mutation {
     createUser(name: String!, email: String!, age: Int): User
     updateUser(id: ID!, name: String, email: String, age: Int): User
     deleteUser(id: ID!): User
-    createPost(title: String!, content: String!, author: String): Post
-    updatePost(id: ID!, title: String, content: String, author: String): Post
-    deletePost(id: ID!): Post
   }
 `;
 
